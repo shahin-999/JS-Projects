d72efeaeb6c9a697b98a0bd563e1a7ce0088f70e
Md Shahin Shanaous <52136857+shahin1998@users.noreply.github.com>
2021-10-15 15:30:32 +0600

    m

---
+++ Birthday-coundown/app.js
@@ -0,0 +1,10 @@
+console.log('hello');
+const bDay = "16 October 2021" ;
+
+//function coundown(){
+    const bDay = new Date(bDay);
+    const day = new Date();
+    const dayRemain = (bDay-day)/1000;
+    console.log(dayRemain);
+document.getElementById('day').innerHTML = dayRemain;
+
